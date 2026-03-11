d3c6be28b1ec43cd77b3fac608f956813ecdee08
Matthieu Gagné <mathdaman@gmail.com>
2026-03-10 20:35:19 -0400

    feat: add music_data.h with BANKREF_EXTERN for song data

---
+++ src/music_data.h
@@ -0,0 +1,11 @@
+#ifndef MUSIC_DATA_H
+#define MUSIC_DATA_H
+
+#include <gb/gb.h>
+#include "banking.h"
+#include "hUGEDriver.h"
+
+BANKREF_EXTERN(music_data_song)
+extern const hUGESong_t music_data_song;
+
+#endif /* MUSIC_DATA_H */
